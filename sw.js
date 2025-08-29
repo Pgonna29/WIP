0a342ab9b285908c1c5ce1e2785061a49cae5f70
Pgonna29 <tanakonlove433@gmail.com>
2025-08-29 23:22:02 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+
+const CACHE_NAME = 'wipfg-cache-v1';
+const APP_SHELL = [
+  './',
+  './index_pwa.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  const url = new URL(request.url);
+
+  // Cache-first for same-origin, network-first for cross-origin (CDNs, Firebase SDK)
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.match(request).then(cached => cached || fetch(request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+        return resp;
+      }))
+    );
+  } else {
+    event.respondWith(
+      fetch(request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+        return resp;
+      }).catch(() => caches.match(request))
+    );
+  }
+});
